ab70c98e2f05fdc85bd7232f82fb5f5ed46fe47b
JagadishMohanty2 <jagadish.mohanty@iesmail.com>
2020-09-13 13:55:28 +0200

    Now the program is able to point to the place the coordinates are pointing
    BUT it still points to the ISP...

--- find_location.py
+++ find_location.py
@@ -1,5 +1,18 @@
+# importing necessary libraries 
+import reverse_geocoder as rg 
 import geocoder
 
 ipadderess = geocoder.ip('me') # get the ip address
 
-print(f"Latitude: {ipadderess.lat}\nLogitude: {ipadderess.lng}")
+#print(f"Latitude: {ipadderess.lat}\nLogitude: {ipadderess.lng}"
+
+def reverseGeocode(coordinates): 
+    result = rg.search(coordinates) # get the location
+    
+    for i in result:
+        print(i,end="\n")
+  
+# The codinates of the IP adress and where it pints to
+coordinates =(ipadderess.lat, ipadderess.lng) 
+
+reverseGeocode(coordinates)
